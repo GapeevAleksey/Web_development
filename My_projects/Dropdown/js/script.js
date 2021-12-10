67be8ebce184ec889986077c2e9fb68d3f8b2957
Gapeev <dizzikz@gmail.com>
2021-12-10 14:12:39 +0300

    Defaul commit

--- My_projects/Dropdown/js/script.js
+++ My_projects/Dropdown/js/script.js
@@ -1,4 +1,5 @@
-let textbox = document.querySelector('.textbox'),
+let dropdown = document.querySelector('.dropdown'),
+  textbox = document.querySelector('.textbox'),
   options = document.querySelector('.options');
 
 getElementDropdownList();
@@ -8,15 +9,18 @@ console.log(options.children.length);
 // --------- Получение выбранного элемента из выпадающего списка ---------------
 
 function getElementDropdownList() {
+  textbox.addEventListener('click', function () {
+    dropdown.classList.toggle('active');
+  });
   for (let i = 0; i < options.children.length; i++) {
     options.children[i] = addEventListener('mousedown', function (event) {
       if (
         event.target.tagName == 'DIV' &&
-        event.target.parentNode.className == options.className &&
-        textbox.value != event.target.textContent
+        event.target.parentNode.className == options.className
+        // && textbox.value != event.target.textContent
       ) {
         textbox.value = event.target.textContent;
-
+        document.querySelector('.dropdown').classList.toggle('active');
         console.log(textbox.value);
       }
       // console.dir(event.target);
