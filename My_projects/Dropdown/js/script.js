68740026beb80d4aca632feab2f504e20f7175df
Gapeev <dizzikz@gmail.com>
2021-12-10 09:36:35 +0300

    Defaul commit

--- My_projects/Dropdown/js/script.js
+++ My_projects/Dropdown/js/script.js
@@ -1,10 +1,22 @@
 let textbox = document.querySelector('.textbox'),
   options = document.querySelector('.options');
 
+getElementDropdownList();
+
 console.log(options.children.length);
 
-for (let i = 0; i < options.children.length; i++) {
-  options.children[i] = addEventListener('click', function (event) {
-    textbox.value = event.target.textContent;
-  });
+// --------- Получение выбранного элемента из выпадающего списка ---------------
+
+function getElementDropdownList() {
+  for (let i = 0; i < options.children.length; i++) {
+    options.children[i] = addEventListener('mousedown', function (event) {
+      if (
+        event.target.tagName == 'DIV' &&
+        event.target.parentNode.className == options.className
+      ) {
+        textbox.value = event.target.textContent;
+      }
+      // console.dir(event.target);
+    });
+  }
 }
