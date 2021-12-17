4a7e80f89e0c1da000401105a7477c8698cb42bc
Gapeev <dizzikz@gmail.com>
2021-12-17 15:55:13 +0300

    Default commit

--- Course_Udemy_JS_React/project/js/script.js
+++ Course_Udemy_JS_React/project/js/script.js
@@ -26,23 +26,40 @@ const movieDB = {
   ],
 };
 
-const promoAdv = document.querySelector('.promo__adv'),
+const promoAdv = document.querySelectorAll('.promo__adv img'),
   promoGenre = document.querySelector('.promo__genre'),
   promoBg = document.querySelector('.promo__bg'),
   promoInteractiveList = document.querySelector('.promo__interactive-list');
 
-promoAdv.remove();
+promoAdv.forEach((item) => item.remove());
+
 promoGenre.textContent = 'Драма';
 promoBg.style.cssText = 'background: url("img/bg.jpg") 0 0 / cover no-repeat';
 promoInteractiveList.innerHTML = '';
 
 movieDB.movies.sort();
-for (let i = 0; i < movieDB.movies.length; i++) {
-  let film = document.createElement('li');
-  film.classList.add('promo__interactive-item');
-  film.textContent = `${i + 1}. ${movieDB.movies[i]}`;
-  promoInteractiveList.append(film);
-}
+
+movieDB.movies.forEach((item, index) => {
+  promoInteractiveList.innerHTML += `
+  <li class="promo__interactive-item">${index + 1}. ${item}
+  <div class="delete"></div>
+</li>
+    `;
+});
+
+// movieDB.movies.forEach((item, index) => {
+//   let film = document.createElement('li');
+//   film.classList.add('promo__interactive-item');
+//   film.textContent = `${index + 1}. ${item}`;
+//   promoInteractiveList.append(film);
+// });
+
+// for (let i = 0; i < movieDB.movies.length; i++) {
+//   let film = document.createElement('li');
+//   film.classList.add('promo__interactive-item');
+//   film.textContent = `${i + 1}. ${movieDB.movies[i]}`;
+//   promoInteractiveList.append(film);
+// }
 
 // for (let key of movieDB.movies) {
 //   let film = document.createElement('li');
