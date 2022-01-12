2da58c768b4eec4d11c3f2547feee21362eaca7e
Gapeev_A_A <dizzikz@gmail.com>
2022-01-12 23:17:55 +0300

    Home commit

---
+++ Training/Fetch_XHR/js/fetch.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const requestURL = 'https://jsonplaceholder.typicode.com/users';
+
+function sendRequest(method, url, body = null) {
+  return fetch(url, {
+    method: method,
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify(body),
+  })
+    .then((data) => {
+      if (!data.ok) {
+        return data.json();
+      }
+      return data.json().then((error) => {
+        const e = new Error('OOooppSSss!!!');
+        e.data = error;
+        console.log(e);
+        throw e;
+      });
+    })
+    .catch((data) => data.json());
+}
+const body = {
+  name: 'Aleksei',
+  age: 35,
+};
+// sendRequest('GET', requestURL)
+//   .then((answer) => console.log(answer))
+//   .catch(() => console.log('OOOppPSSSs!!'));
+
+sendRequest('POST', requestURL, body)
+  .then((answer) => console.log(answer))
+  .catch(() => console.log('OOOppPSSSs!!'));
