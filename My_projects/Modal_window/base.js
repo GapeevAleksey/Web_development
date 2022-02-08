9258ea85b9759e55f5ca792fe04a401f3c1844a4
Gapeev <dizzikz@gmail.com>
2022-02-08 15:35:24 +0300

    Default commit

--- My_projects/Modal_window/base.js
+++ My_projects/Modal_window/base.js
@@ -5,4 +5,22 @@ const options = {
 	content: `<p>Lorem ipsum dolor sit amet.</p>
   <p>Lorem ipsum dolor sit amet.</p>`,
 	width: '700px',
+	footerButtons: [
+		{
+			text: 'Ok',
+			type: 'primary',
+			handler() {
+				modal.close();
+				console.log('Primary btn is clicked!');
+			},
+		},
+		{
+			text: 'Cancel',
+			type: 'danger',
+			handler() {
+				modal.close();
+				console.log('Danger btn is clicked!');
+			},
+		},
+	],
 };
--- My_projects/Modal_window/plugins/modal.js
+++ My_projects/Modal_window/plugins/modal.js
@@ -1,3 +1,27 @@
+Element.prototype.appendAfter = function (element) {
+	element.parentNode.insertBefore(this, element.nextSibling);
+};
+function noop() {}
+
+function _createModalFooter(buttons = []) {
+	if (buttons.length === 0) {
+		return document.createElement('div');
+	}
+	const wrap = document.createElement('div');
+	wrap.classList.add('modal-footer');
+
+	buttons.forEach((btn) => {
+		const $btn = document.createElement('button');
+		$btn.textContent = btn.text;
+		$btn.classList.add('btn');
+		$btn.classList.add(`btn-${btn.type || 'secondary'}`);
+		$btn.onclick = btn.handler || noop;
+		wrap.appendChild($btn);
+	});
+
+	return wrap;
+}
+
 function _createModal(options) {
 	const DEFAULT_WIGTH = '600px';
 	const modal = document.createElement('div');
@@ -10,18 +34,15 @@ function _createModal(options) {
           <span class="modal-title">${options.title || 'The title is not defined!'}</span>
          ${options.closable ? `<span class="modal-close">&times</span>` : ''}
         </div>
-       <div class="modal-body">
+       <div class="modal-body" data-content>
          ${options.content || ''}
-        </div>
-        <div class="modal-footer">
-          <button>Ok</button>
-          <button>Cancel</button>
         </div>
       </div>
     </div>`
 	);
 	// options.closable && modal.querySelector('.modal-close').classList.add('show');
-
+	const footer = _createModalFooter(options.footerButtons);
+	footer.appendAfter(modal.querySelector('[data-content]'));
 	document.body.appendChild(modal);
 
 	return modal;
@@ -64,6 +85,10 @@ $.modal = function (options) {
 		destroy() {
 			$modal.outerHTML = '';
 			$modal.removeEventListener('click', listener);
+		},
+		setContent(data) {
+			console.log($modal);
+			$modal.querySelector('[data-content]').innerHTML = data;
 		},
 	});
 };
